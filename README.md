947b60edcee4a06539dd62fa5ee12293e7befb76
kamakuni <superradio86@gmail.com>
2020-03-17 23:29:16 +0900

    add Print method to show Emurator status

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # chip8
+http://www.multigesture.net/articles/how-to-write-an-emulator-chip-8-interpreter/
--- main.go
+++ main.go
@@ -18,6 +18,33 @@ type Emulator struct {
 	key        [16]uint8     // to store current stats of key
 }
 
+// Create Emulator
+func NewEmulator() *Emulator {
+	return &Emulator{
+		pc:     0x200,
+		opcode: 0,
+		i:      0,
+		sp:     0,
+	}
+}
+
+// Print Emulator status
+func (e Emulator) Print() {
+	fmt.Printf("opcode:%v\n", e.opcode)
+	fmt.Printf("memory:%v\n", e.memory)
+	fmt.Printf("v:%v\n", e.v)
+	fmt.Printf("i:%v\n", e.i)
+	fmt.Printf("pc:%v\n", e.pc)
+	fmt.Printf("gfx:%v\n", e.gfx)
+	fmt.Printf("delayTimer:%v\n", e.delayTimer)
+	fmt.Printf("soundTimer:%v\n", e.soundTimer)
+	fmt.Printf("stack:%v\n", e.stack)
+	fmt.Printf("sp:%v\n", e.sp)
+	fmt.Printf("key:%v\n", e.key)
+}
+
 func main() {
 	fmt.Println("start")
+	emu := NewEmulator()
+	emu.Print()
 }
