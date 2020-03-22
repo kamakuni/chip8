12a50798f1fb955b59f1647ce254029944bfd1ee
kamakuni <superradio86@gmail.com>
2020-03-22 20:58:24 +0900

    add testing code when Emurator.Decode opcode 0xANNN

--- main_test.go
+++ main_test.go
@@ -14,5 +14,20 @@ func TestEmulator_Fetch(t *testing.T) {
 	if actual != expected {
 		t.Errorf("got: 0x%x,but expected: 0x%x", actual, expected)
 	}
+}
 
+func TestEmulator_Decode0xANNN(t *testing.T) {
+	fonts := NewFonts()
+	emu := NewEmulator(fonts)
+	data := make([]byte, 2)
+	data[0] = 0xA2
+	data[1] = 0xF0
+	emu.Load(data)
+	opcode := emu.Fetch()
+	emu.Decode(opcode)
+	actual := emu.I
+	expected := uint16(0x02F0)
+	if actual != expected {
+		t.Errorf("got: 0x%x,but expected: 0x%x", actual, expected)
+	}
 }
